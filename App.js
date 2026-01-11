3fc7e315ef3c91c24e58553beb1e841336922b2b
dennie <denitsa.sureva@icloud.com>
2026-01-11 12:12:59 +0100

    initializing react native project

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View >
+      <Text>Hello</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
